1f0e974b008acf6432def7540fe393065596ea39
Ramesh Parthasarathy <msg.rameshp@gmail.com>
2017-04-06 22:23:49 -0400

    Initial commit

---
+++ On the Spot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  On the Spot
+//
+//  Created by Ramesh Parthasarathy on 4/6/17.
+//  Copyright © 2017 Ramesh Parthasarathy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
