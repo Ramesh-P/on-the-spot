06b1947b6387ca07a3775a93db58dfb0746d1f09
Ramesh Parthasarathy <msg.rameshp@gmail.com>
2017-04-11 12:32:53 -0400

    feat: Add storyboard view controllers and configure
    - Add navigation controller
    - Configure status bar and navigation bar appearance
    - Associate places class & view controller

--- On the Spot/AppDelegate.swift
+++ On the Spot/AppDelegate.swift
@@ -13,9 +13,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // Hide navigation bar border
+        UINavigationBar.appearance().shadowImage = UIImage()
+        UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
+        
         return true
     }
 
@@ -40,7 +43,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
 
--- On the Spot/PlacesViewController.swift
+++ On the Spot/PlacesViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  PlacesViewController.swift
 //  On the Spot
 //
 //  Created by Ramesh Parthasarathy on 4/6/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PlacesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
