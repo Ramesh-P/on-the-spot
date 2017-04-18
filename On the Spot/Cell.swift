44e5c78799e25a9762091991fe3bfdeb209bedeb
Ramesh Parthasarathy <msg.rameshp@gmail.com>
2017-04-17 23:13:17 -0400

    feat: Configure places view controller
    - Update places view image asset catalog

---
+++ On the Spot/Cell.swift
@@ -0,0 +1,21 @@
+//
+//  Cell.swift
+//  On the Spot
+//
+//  Created by Ramesh Parthasarathy on 4/16/17.
+//  Copyright © 2017 Ramesh Parthasarathy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// MARK: Cell
+class Cell: UICollectionViewCell {
+    
+    // MARK: Outlets
+    @IBOutlet weak var placesImageFrame: UIView!
+    @IBOutlet weak var placesTextFrame: UIView!
+    @IBOutlet weak var placeIcon: UIImageView!
+    @IBOutlet weak var placeType: UILabel!
+}
+
